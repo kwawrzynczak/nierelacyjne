f42872be5124c0cec2073552ab025c991eb63d7c
Piggey <pepcio3320@gmail.com>
2022-10-15 18:23:08 +0200

    klasa dziecka

--- model/Child.py
+++ model/Child.py
@@ -1,3 +1,29 @@
 
 class Child():
-    pass
+    """Dziecko, dla którego będzie wynajmowana opiekunka"""
+    child_name: str
+    child_age: int 
+
+    def __init__(self, name: str, age: int):
+        if not name or not name.strip():
+            raise ValueError("Wprowadz poprawne imie dziecka!")
+
+        if age < 0:
+            raise ValueError("Wprowadz poprawny wiek dziecka!")
+
+        self.child_age = age
+        self.child_name = name
+
+    def get_child_info(self) -> str:
+        out = ''
+        out += f'Imie dziecka: {self.child_name}\n'
+        out += f'Wiek dziecka: {self.child_age}'
+
+        if self.child_age == 1:
+            out += ' rok\n'
+        elif self.child_age in [2, 3, 4]:
+            out += ' lata\n'
+        else:
+            out += ' lat\n'
+
+        return out
