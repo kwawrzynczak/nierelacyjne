b30dc36358d2fcabe4e0fccf6bb9245eb386f3c1
Piggey <pepcio3320@gmail.com>
2022-10-15 20:41:52 +0200

    klasa pomocy domowej

--- model/Housekeeper.py
+++ model/Housekeeper.py
@@ -1,4 +1,21 @@
 from Sitter import Sitter
 
 class Housekeeper(Sitter):
-    pass
+    """Klasa pomocy domowej"""
+    
+    def __init__(self, sitter_id: int, first_name: str, last_name: str, base_price: float):
+        super().__init__(sitter_id, first_name, last_name, base_price)
+
+    def get_actual_price(self) -> float: 
+        return self.base_price
+
+    def get_sitter_info(self) -> str: 
+        out = f'ID opiekunki: {self.sitter_id}\n'
+        out += f'Opiekunka {self.first_name} {self.last_name}\n'
+        out += 'Pomoc domowa\n'
+        out += f'Cena podstawowa: {self.base_price}\n'
+
+        return out
+
+    def get_max_age(self) -> int:
+        return 0
