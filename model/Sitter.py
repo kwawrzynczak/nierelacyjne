ea2f0d2a99ac8dbf35390845663dc0874db3ed96
Piggey <pepcio3320@gmail.com>
2022-10-15 20:41:42 +0200

    klasa bazowa opiekunki

--- model/Sitter.py
+++ model/Sitter.py
@@ -1,3 +1,34 @@
 
 class Sitter():
-    pass
+    """Bazowa klasa opiekunki dla dzieci"""
+
+    sitter_id: int
+    first_name: str
+    last_name: str 
+    base_price: float 
+    is_available: bool 
+
+    def __init__(
+        self, 
+        sitter_id: int, 
+        first_name: str, 
+        last_name: str,
+        base_price: float
+    ):
+        if not first_name or not first_name.split():
+            raise ValueError("Wprowadz poprawne imie opiekunki!")
+
+        if not last_name or not last_name.split():
+            raise ValueError("Wprowadz poprawne nazwisko opiekunki!")
+
+        if base_price <= 0:
+            raise ValueError("Cena podstawowa powinna byc wieksza od zera.")
+
+        self.sitter_id = sitter_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.base_price = base_price
+
+    def set_available(self, is_available: bool):
+        self.is_available = is_available
+        
