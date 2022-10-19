de1fa2472de16e8d3990ead7229fb99104d6d494
236688 Kinga Wawrzyńczak <236688@edu.p.lodz.pl>
2022-10-19 19:59:32 +0200

    ParentManager

--- ParentManager.py
+++ ParentManager.py
@@ -1,8 +1,30 @@
+from Child import Child
 from Parent import Parent
+from Repository import Repository
+
 
 class ParentManager():
-    def create_parent(parent_id, parent_name, address, phone_number, is_teaching_required, child):
-        parent = Parent()
+    def __init__(self):
+        self.repo = Repository()
 
+    def create_parent(
+            self,
+            parent_name: str,
+            address: str,
+            phone_number: str,
+            is_teaching_required: bool,
+            child: Child
+    ):
 
+        parent = Parent(parent_name, address, phone_number, is_teaching_required, child)
+        self.repo.add(parent)
+        return True
 
+    def get_parent(self, parent_id: int) -> Parent:
+        return self.repo.get(Parent, parent_id)
+
+    def find_parents(self, predicate) -> list[Parent]:
+        return self.repo.find_by(Parent, predicate)
+
+    def get_all_parents(self) -> list[Parent]:
+        return self.repo.find_all(Parent)
