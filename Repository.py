e9f0146edda39ad4abf332d3e09a42c55d5438ac
Piggey <pepcio3320@gmail.com>
2022-10-19 18:58:01 +0200

    unifikacja id

--- Repository.py
+++ Repository.py
@@ -2,11 +2,11 @@ from DatabaseSession import DatabaseSession
 from Base import Base
 
 class Repository():
+    """klasa zajmujaca sie obsluga danych z bazy danych"""
 
     def __init__(self):
         self.ds = DatabaseSession()
         
-
     def add(self, obj: Base):
         db = self.ds.create_new_session()
         db.add(obj)
@@ -34,5 +34,9 @@ class Repository():
         return result
 
     def find_by(self, class_type: type, predicate) -> list[Base]:
-        pass
+        db = self.ds.create_new_session()
+        result = db.query(class_type).where(predicate)
+        db.close()
+
+        return result
 
--- create_model.py
+++ create_model.py
@@ -8,24 +8,24 @@ meta = MetaData()
 
 children = Table(
     'children', meta,
-    Column('child_id', Integer, autoincrement=True, primary_key=True),
+    Column('id', Integer, autoincrement=True, primary_key=True),
     Column('child_name', String(50), nullable=False),
     Column('child_age', Integer, nullable=False)
 )
 
 parents = Table(
     'parents', meta,
-    Column('parent_id', Integer, autoincrement=True, primary_key=True),
+    Column('id', Integer, autoincrement=True, primary_key=True),
     Column('parent_name', String(50), nullable=False),
     Column('address', String(255)),
     Column('phone_number', String(12), nullable=False),
     Column('is_teaching_required', Boolean, default=false),
-    Column('child_id', ForeignKey('children.child_id'))
+    Column('child_id', ForeignKey('children.id'))
 )
 
 sitters = Table(
     'sitters', meta,
-    Column('sitter_id', Integer, autoincrement=True, primary_key=True),
+    Column('id', Integer, autoincrement=True, primary_key=True),
     Column('first_name', String(50)),
     Column('last_name', String(50), nullable=False),
     Column('base_price', Float, nullable=False),
@@ -34,14 +34,14 @@ sitters = Table(
 
 housekeepers = Table(
     'housekeepers', meta,
-    Column('housekeeper_id', Integer, autoincrement=True, primary_key=True),
-    Column('sitter_id', ForeignKey('sitters.sitter_id'))
+    Column('id', Integer, autoincrement=True, primary_key=True),
+    Column('sitter_id', ForeignKey('sitters.id'))
 )
 
 academic_sitters = Table(
     'academic_sitters', meta,
-    Column('academic_sitter_id', Integer, autoincrement=True, primary_key=True),
-    Column('sitter_id', ForeignKey('sitters.sitter_id')),
+    Column('id', Integer, autoincrement=True, primary_key=True),
+    Column('sitter_id', ForeignKey('sitters.id')),
     Column('bonus', Float),
     Column('max_age', Integer)
 )
