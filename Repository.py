5330fe0098b2697066144edff5e2d96bc79a9adc
Piggey <pepcio3320@gmail.com>
2022-10-19 15:34:57 +0200

    repozytorium korzysta z klasy `DatabaseSession`

--- Repository.py
+++ Repository.py
@@ -1,41 +1,30 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
-
-Base = declarative_base()
+from DatabaseSession import DatabaseSession
+from Base import Base
 
 class Repository():
 
     def __init__(self):
-        dialect = 'postgresql'
-        driver = 'psycopg2'
-        username = 'nbd'
-        password = 'nbdpassword'
-        host = 'localhost'
-        port = 5432
-        database = 'nbddb'
-
-        self.engine = create_engine(
-            f'{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}',
-            echo=True
-        )
-
-        self.session_creator = sessionmaker(bind=self.engine)
+        self.ds = DatabaseSession()
         
 
     def add(self, obj: Base):
-        db = self.session_creator()
+        db = self.ds.create_new_session()
         db.add(obj)
         db.commit()
-
-        print(db.query(type(obj)).count())
         db.close()
 
     def remove(self, obj: Base):
-        db = self.session_creator()
+        db = self.ds.create_new_session()
         db.delete(obj)
+        db.commit()
         db.close()
 
-repo = Repository()
+    def get(self, class_type: type, id: int) -> Base:
+        pass
 
+    def find_all(self, class_type: type) -> list[Base]:
+        pass
+
+    def find_by(self, class_type: type, predicate) -> list[Base]:
+        pass
+    
