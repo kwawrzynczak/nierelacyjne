05abe67aa494ec3139ded5276393b2911177e05e
Piggey <pepcio3320@gmail.com>
2022-11-09 17:08:56 +0100

    repozytorium i lacznosc z baza danych

--- src/Repository.py
+++ src/Repository.py
@@ -1,41 +1,39 @@
-from DatabaseSession import DatabaseSession
-from Base import Base
+from DatabaseClient import DatabaseClient
+
+from uuid import UUID
 
 class Repository():
     """klasa zajmujaca sie obsluga danych z bazy danych"""
 
     def __init__(self):
-        self.ds = DatabaseSession()
+        self.dc = DatabaseClient()
         
-    def add(self, obj: Base):
-        db = self.ds.create_new_session()
-        db.add(obj)
-        db.commit()
-        db.close()
+    def add(self, collection_name: str, document: dict) -> UUID | None:
+        collection = self.dc.db[collection_name]
+        ins_id = collection.insert_one(document).inserted_id
 
-    def remove(self, obj: Base):
-        db = self.ds.create_new_session()
-        db.delete(obj)
-        db.commit()
-        db.close()
+        return ins_id
 
-    def get(self, class_type: type, id: int) -> Base:
-        db = self.ds.create_new_session()
-        result = db.query(class_type).where(class_type.id == id)
-        db.close()
+    def remove(self, collection_name: str, document: dict) -> int:
+        collection = self.dc.db[collection_name]
+        del_count = collection.delete_one({ '_id': document['_id'] }).deleted_count
+
+        return del_count
+
+    def get(self, collection_name: str, id: str) -> dict | None:
+        collection = self.dc.db[collection_name]
+        result = collection.find_one({ '_id': id })
         
         return result
 
-    def find_all(self, class_type: type) -> list[Base]:
-        db = self.ds.create_new_session()
-        result = db.query(class_type).all()
-        db.close()
+    def find_all(self, collection_name: str) -> list[dict]:
+        collection = self.dc.db[collection_name]
+        result = collection.find()
 
         return result
 
-    def find_by(self, class_type: type, predicate) -> list[Base]:
-        db = self.ds.create_new_session()
-        result = db.query(class_type).where(predicate)
-        db.close()
+    def find_by(self, collection_name: str, filter) -> list[dict]:
+        collection = self.dc.db[collection_name]
+        result = collection.find(filter)
 
         return result
