3c08889c2a863aab5a7b1672997a8f9dc4a04c68
Piggey <pepcio3320@gmail.com>
2022-11-23 16:21:09 +0100

    interakcja z baza danych

--- src/DatabaseClient.py
+++ src/DatabaseClient.py
@@ -1,29 +1,19 @@
-from pymongo import MongoClient
+import redis
+from redis.commands.json.path import Path
 
 class DatabaseClient():
     """Klient bazy danych"""
 
+    db: redis.Redis
+    json_root: Path
+
     def __init__(self):
-        client = MongoClient(connection_string)
+        self.json_root = Path.root_path()
 
-        # dostep do bazy dantch o nazwie 'nierelacyjne'
-        self.db = client['nierelacyjne']
+        self.db = redis.Redis(
+            host='localhost',
+            port=6379,
+        )
 
-
-import redis
-from redis.commands.json.path import Path
-from Sitter import Sitter
-
-r = redis.Redis(
-    host='localhost',
-    port=6379,
-)
-
-s = Sitter('first_name', 'last_name', 1300)
-
-r.set('foo', 12)
-# r.set('foo', s.as_dict())
-r.json().set('sitter:1', Path.root_path(), s.as_dict())
-value = r.json().get('sitter:1')
-
-print(value)
+class DatabaseError(Exception):
+    pass
--- src/Repository.py
+++ src/Repository.py
@@ -1,39 +1,64 @@
-from DatabaseClient import DatabaseClient
-
-from uuid import UUID
+from DatabaseClient import DatabaseClient, DatabaseError
+from redis import ResponseError
 
 class Repository():
-    """klasa zajmujaca sie obsluga danych z bazy danych"""
+    """klasa zajmujaca sie obsluga danych z bazy danych
+        Przedrostki klucza (w zaleznosci od klasy):
+            - sitter
+            - housekeeper
+            - academic
+            - child
+            - parent
+            - reservation
+
+        Przyklad: sitter:1 -> pokojowka z _id = 1
+    """
 
     def __init__(self):
         self.dc = DatabaseClient()
         
-    def add(self, collection_name: str, document: dict) -> UUID | None:
-        collection = self.dc.db[collection_name]
-        ins_id = collection.insert_one(document).inserted_id
+    def add(self, name: str, value: object, overwrite: bool = False):
+        if type(value) == dict: # dodajemy nowy obiekt klasy w postaci json
+            result = self.dc.db.json().set(name, self.dc.json_root, value)
+        else: # cos innego
+            result = self.dc.db.set(name, value, nx=not overwrite)
 
-        return ins_id
+        if result == None or result == False:
+            raise DatabaseError('Failed to add new item to the database.')
 
-    def remove(self, collection_name: str, document: dict) -> int:
-        collection = self.dc.db[collection_name]
-        del_count = collection.delete_one({ '_id': document['_id'] }).deleted_count
+    def remove(self, name: str) -> int:
+        json = self.dc.db.json().delete(name)
+        return self.dc.db.delete(name) + json
 
-        return del_count
+    def get(self, name: str) -> object:
+        try:
+            result = self.dc.db.get(name)
+        except ResponseError: # chcemy dostac obiekt klasy
+            result = self.dc.db.json().get(name)
 
-    def get(self, collection_name: str, id: str) -> dict | None:
-        collection = self.dc.db[collection_name]
-        result = collection.find_one({ '_id': id })
-        
+        if result == None:
+            raise DatabaseError(f'Failed to retrieve {name}. Pair does not exist.')
+
         return result
 
-    def find_all(self, collection_name: str) -> list[dict]:
-        collection = self.dc.db[collection_name]
-        result = collection.find()
+    def find_all(self, prefix: str) -> list[dict]:
+        _, keys = self.dc.db.scan(match=prefix)
 
-        return list(result)
+        return self.dc.db.json().mget(keys, self.dc.json_root)
 
-    def find_by(self, collection_name: str, filter) -> list[dict]:
-        collection = self.dc.db[collection_name]
-        result = collection.find(filter)
+    def find_by(self, prefix: str, predicate: dict) -> list[dict]:
+        _, keys = self.dc.db.scan(match=prefix)
 
-        return list(result)
+        out = []
+        for obj in self.dc.db.json().mget(keys, self.dc.json_root):
+            matching = True
+
+            for key, value in predicate.items():
+                if obj[key] != value:
+                    matching = False
+                    break
+
+            if matching:
+                out.append(obj)
+        
+        return out
