ac64c76830c68d2135e10311765f659e30916113
Piggey <pepcio3320@gmail.com>
2022-11-23 14:52:37 +0100

    literalnie cale zadanie lol

--- src/DatabaseClient.py
+++ src/DatabaseClient.py
@@ -8,3 +8,22 @@ class DatabaseClient():
 
         # dostep do bazy dantch o nazwie 'nierelacyjne'
         self.db = client['nierelacyjne']
+
+
+import redis
+from redis.commands.json.path import Path
+from Sitter import Sitter
+
+r = redis.Redis(
+    host='localhost',
+    port=6379,
+)
+
+s = Sitter('first_name', 'last_name', 1300)
+
+r.set('foo', 12)
+# r.set('foo', s.as_dict())
+r.json().set('sitter:1', Path.root_path(), s.as_dict())
+value = r.json().get('sitter:1')
+
+print(value)
