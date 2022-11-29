d7826554d960b36f98ffc18a1e39d4dd846b551b
Piggey <pepcio3320@gmail.com>
2022-11-29 22:00:03 +0100

    rozdzielenie na cache i db

--- src/Repository.py
+++ src/Repository.py
@@ -6,34 +6,59 @@ class Repository():
     """klasa zajmujaca sie obsluga danych z bazy danych"""
 
     def __init__(self):
-        self.dc = DatabaseClient()
-        
-    def add(self, collection_name: str, document: dict) -> UUID | None:
-        collection = self.dc.db[collection_name]
-        ins_id = collection.insert_one(document).inserted_id
+        dc = DatabaseClient()
 
-        return ins_id
+        self.db = dc.mongo_db
+        self.cache = dc.redis_db
+        self.root = dc.cache_root
+        
+    def add(self, collection_name: str, document: dict, use_cache: bool = True) -> UUID | None:
+        inserted_id = self._add_to_db(collection_name, document)
+        if use_cache: self._add_to_cache(f'{collection_name}:{document["_id"]}', document)
+
+        return inserted_id
 
     def remove(self, collection_name: str, document: dict) -> int:
-        collection = self.dc.db[collection_name]
-        del_count = collection.delete_one({ '_id': document['_id'] }).deleted_count
+        collection = self.db[collection_name]
+        return collection.delete_one({ '_id': document['_id'] }).deleted_count
 
-        return del_count
+    def get(self, collection_name: str, _id: str, use_cache: bool = True) -> dict | None:
+        if not use_cache:
+            return self._get_from_db(collection_name, _id)
 
-    def get(self, collection_name: str, id: str) -> dict | None:
-        collection = self.dc.db[collection_name]
-        result = collection.find_one({ '_id': id })
-        
-        return result
+        from_cache = self._get_from_cache(f'{collection_name}:{id}')
+        if not from_cache: # tried to get from cache but failed
+            doc = self._get_from_db(collection_name, _id)
+            self._add_to_cache(f'{collection_name}:{_id}', doc)
+
+        return doc
 
     def find_all(self, collection_name: str) -> list[dict]:
-        collection = self.dc.db[collection_name]
-        result = collection.find()
+        collection = self.db[collection_name]
+        return list(collection.find())
 
-        return list(result)
+    def find_by(self, collection_name: str, filter: dict) -> list[dict]:
+        collection = self.db[collection_name]
+        return list(collection.find(filter))
 
-    def find_by(self, collection_name: str, filter) -> list[dict]:
-        collection = self.dc.db[collection_name]
-        result = collection.find(filter)
+    def _add_to_db(self, collection_name: str, document: dict) -> UUID | None:
+        collection = self.db[collection_name]
+        return collection.insert_one(document).inserted_id
 
-        return list(result)
+    def _add_to_cache(self, cache_key: str, document: dict, expire_seconds: int = DatabaseClient.CACHE_EXPIRE_SECONDS) -> bool:
+        print(f'adding {cache_key} to cache. data will expire after {expire_seconds} seconds')
+        rslt = self.cache.json().set(cache_key, self.root, document)
+        self.cache.expire(cache_key, expire_seconds)
+        return rslt
+
+    def _get_from_cache(self, cache_key: str) -> dict | None:
+        rslt = self.cache.json().get(cache_key)
+        
+        if rslt: print(f'retrieved {cache_key} from cache')
+        else: print(f'failed to retrieve {cache_key} from cache')
+        
+        return rslt
+
+    def _get_from_db(self, collection_name: str, _id: str) -> dict | None:
+        collection = self.db[collection_name]
+        return collection.find_one({ '_id': _id })
