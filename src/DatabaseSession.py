acffbca89c7bb96f0c4ec1160b64a5c4a1628691
Piggey <pepcio3320@gmail.com>
2022-12-29 22:48:52 +0100

    funkcjonalnosc bazy danych

--- src/DatabaseSession.py
+++ src/DatabaseSession.py
@@ -1,6 +1,9 @@
 from cassandra.cluster import Cluster, ExecutionProfile, ConsistencyLevel, EXEC_PROFILE_DEFAULT
-from cassandra.policies import DowngradingConsistencyRetryPolicy
+from cassandra.policies import LoadBalancingPolicy
 from cassandra.query import tuple_factory
+from uuid import UUID
+
+from DatabaseObject import DatabaseObject
 
 class DatabaseSession():
     """Fabryka sesji korzystania z bazy danych"""
@@ -10,12 +13,11 @@ class DatabaseSession():
             '172.17.0.2',
             '172.17.0.3',
         ]
-
         STRATEGY = 'SimpleStrategy'
         REP_FACTOR = 2
 
         exec_profile = ExecutionProfile(
-            retry_policy=DowngradingConsistencyRetryPolicy(),
+            retry_policy=LoadBalancingPolicy(),
             consistency_level=ConsistencyLevel.LOCAL_QUORUM,
             serial_consistency_level=ConsistencyLevel.LOCAL_SERIAL,
             request_timeout=15,
@@ -39,3 +41,27 @@ class DatabaseSession():
         )
 
         self.session.set_keyspace('nierelacyjne')
+        
+    def add(self, obj: DatabaseObject):
+        self.session.execute(obj.add_to_database())
+
+    def remove(self, obj: DatabaseObject):
+        self.session.execute(obj.delete_from_database())
+
+    def update(self, obj: DatabaseObject):
+        self.session.execute(obj.update_data())
+
+    def get(self, table_name: str, _id: UUID) -> tuple:
+        return self.session.execute(
+            f"""
+            SELECT * FROM {table_name}
+            WHERE {table_name[0]}_id = {str(_id)};
+            """
+        ).one()
+
+    def find_all(self, table_name: str) -> list[tuple]:
+        return list(self.session.execute(f"SELECT * FROM {table_name};"))
+
+    def find_by(self, table_name: str, where_sql_clause: str) -> list[tuple]:
+        return list(self.session.execute(f"SELECT * FROM {table_name} WHERE {where_sql_clause};"))
+        
